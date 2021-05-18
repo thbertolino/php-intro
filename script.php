5ed3c84a3ab3b092eb8b8bedb0a3a8fe2c58e3c6
Thiago Bertolino <thiagopbertolino@gmail.com>
2021-05-18 15:19:21 -0300

    Finalizacao da aula

--- script.php
+++ script.php
@@ -2,65 +2,13 @@
 
 session_start();
 
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php";
+include "servicos/servicoCategoriaCompetidora.php";
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-if(empty($nome)){
-    $_SESSION['mensagem-de-erro'] = "O nome não pode ser vazio, por favor preencha o formulário novamente";
-    header( 'location: index.php' );
-    return;
-}
+defineCategoriaCompetidor($nome, $idade);
 
-else if(strlen($nome)< 3){    
-    $_SESSION['mensagem-de-erro'] = "O nome não pode ter menos que 3 caracteres";
-    header( 'location: index.php' );
-    return;
-}
-
-else if(strlen($nome) > 40){
-    $_SESSION['mensagem-de-erro'] = "O nome não pode ter menos que 40 caracteres";
-    header( 'location: index.php' );
-    return;
-}
-
-else if(!is_numeric($idade)){
-    $_SESSION['mensagem-de-erro'] = "Informe um número para a idade";
-    header( 'location: index.php' );
-    return;
-}
-
-if ($idade >= 6 && $idade <= 12){
-    for ($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == "infantil"){
-            $_SESSION['mensagem-de-erro'] = "O nadador ".$nome. " compete na categoria ".$categorias[$i];
-            header ( 'location: index.php ');
-            return;
-        }
-    }
-}
-else if ($idade >=13 && $idade <= 18){
-    for ($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'adolescente'){
-        $_SESSION['mensagem-de-erro'] = "O nadador ".$nome. " compete na categoria adolescente";
-        header ( 'location: index.php ');
-        return;
-        }
-    }
-}
-else {
-    for ($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'adulto'){
-                        $_SESSION['mensagem-de-erro'] = "O nadador ".$nome. " compete na categoria adulto";
-            header ( 'location: index.php ');
-            return;
-        }
-    }   
-}
+header ('location: index.php');
---
+++ servicos/servicoCategoriaCompetidora.php
@@ -0,0 +1,46 @@
+<?php
+
+function defineCategoriaCompetidor(string $nome, string $idade) : ?string
+{
+    $categorias = [];
+    $categorias[] = 'infantil';
+    $categorias[] = 'adolescente';
+    $categorias[] = 'adulto';
+
+    if(validarNome($nome) && validarIdade($idade))
+    {
+        removerMensagemErro();
+        if ($idade >= 6 && $idade <= 12){
+            for ($i = 0; $i <= count($categorias); $i++)
+            {
+                if($categorias[$i] == "infantil"){
+                    setarMensagemSucesso("O nadador ".$nome. " compete na categoria ".$categorias[$i]);
+                    return null;
+                }
+            }
+        }
+        else if ($idade >=13 && $idade <= 18){
+            for ($i = 0; $i <= count($categorias); $i++)
+            {
+                if($categorias[$i] == 'adolescente'){
+                setarMensagemSucesso("O nadador ".$nome. " compete na categoria adolescente");
+                return null;
+                }
+            }
+        }
+        else {
+            for ($i = 0; $i <= count($categorias); $i++)
+            {
+                if($categorias[$i] == 'adulto'){
+                    setarMensagemSucesso("O nadador ".$nome. " compete na categoria adulto");
+                    return null;
+                }
+            }   
+        }
+    }
+    else 
+    {
+        removerMensagemSucesso();
+        return obterMensagemErro();
+    }
+}

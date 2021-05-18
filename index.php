32a21a6ebeca941974fabd1ae91588600fc8f986
Thiago Bertolino <thiagopbertolino@gmail.com>
2021-05-18 10:41:19 -0300

    Adicionando validação de campos do formulário

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,13 +8,13 @@
     <title>Document</title>
 </head>
 <body>
+
     <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
     <form action="script.php" method="post">
     <p>Seu nome: <input type ="text" name="nome" /></p>
     <p>Sua idade: <input type ="text" name="idade" /></p>
     <p><input type="submit" value="Enviar dados do competidor" /></p>
-    
     </form>
 </body>
 </html>l
--- script.php
+++ script.php
@@ -9,6 +9,26 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+if(empty($nome)){
+    echo "O nome não pode ser vazio";
+    return;
+}
+
+if(strlen($nome)< 3){
+    echo "O nome deve conter mais que 3 caracteres";
+    return;
+}
+
+if(strlen($nome) > 40){
+    echo "O nome é muito extenso";
+    return;
+}
+
+if(!is_numeric($idade)){
+    echo "Informe o número para a idade";
+    return;
+}
+
 if ($idade >= 6 && $idade <= 12){
     for ($i = 0; $i <= count($categorias); $i++)
     {

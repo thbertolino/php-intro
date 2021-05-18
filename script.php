287db15c0742e0990dc365f1eb714419d20367e5
Thiago Bertolino <thiagopbertolino@gmail.com>
2021-05-18 09:53:34 -0300

    Alterações Aula 01

--- script.php
+++ script.php
@@ -9,15 +9,25 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-var_dump($nome);
-var_dump($idade);
-return 0;
-
-// if ($idade >= 6 && $idade <= 12){
-//     for ($i = 0; $i <= count($categorias); i++)
-//     {
-//         if($categorias[$i] == 'infantil')
-//         echo "O nadador ".$nome. " compete na categoria ".$categorias[$i]
-//     }
-// }
-
+if ($idade >= 6 && $idade <= 12){
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+        echo "O nadador ".$nome. " compete na categoria ".$categorias[$i];
+    }
+}
+else if ($idade >=13 && $idade <= 18){
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+        echo "O nadador ".$nome. " compete na categoria adolescente";
+    }
+}
+else {
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+        // if($categorias[$i] == 'adulto')
+        echo "O nadador ".$nome. " compete na categoria adulto";
+    }
+}
